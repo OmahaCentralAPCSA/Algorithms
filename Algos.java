ef0a78dc5865a381e471af3d2aafa57138b00152
Collin Holmquist <collinholmquist@gmail.com>
2020-10-08 14:42:26 -0500

    Create Algos.java

---
+++ Algos.java
@@ -0,0 +1,24 @@
+public class Algos{
+
+  public static void main(String[] args) {
+  
+    
+  }
+  
+  public static int gcd(int a, int b) {
+  
+      return 0; //change this
+  }
+  
+  pubilc static void perfectNumbers() {
+  
+      //print out the first 4 perfect numbers
+  }
+  
+  public static void mersennePrimes() {
+  
+    //print out the first 6 mersenne primes and their corresponding perfect numbers.
+  }
+
+
+}
