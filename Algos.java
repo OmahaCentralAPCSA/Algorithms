6af3be053fd7a3a4c51113c113b21616607213e7
Collin Holmquist <collinholmquist@gmail.com>
2020-10-12 15:19:47 +0000

    gcd and perfect number pseudo-code

--- Algos.java
+++ Algos.java
@@ -2,17 +2,53 @@ public class Algos{
 
   public static void main(String[] args) {
   
-    
+      System.out.println(gcd(3954, 134));
   }
   
   public static int gcd(int a, int b) {
-  
-      return 0; //change this
+
+      while(a != 0 && b != 0) {
+        //swap b into the place of a. 
+        int temp = b; 
+        b = a % b; //remainder of a / b 
+        a = temp; 
+        System.out.println("A: " + a + "B: " + b); 
+      }
+      //either a or b is 0 since our while loop stopped. 
+      if( a == 0) {
+          return b; 
+      } else { //b == 0
+          return a; 
+      }
   }
   
-  pubilc static void perfectNumbers() {
+  public static void perfectNumbers() {
   
       //print out the first 4 perfect numbers
+    //perfNumCount -> keep track of my perfect numbers
+    
+
+    //repeat until perfNumCount == 4:
+      // num -> the number I want to check.  
+      //temp -> one less than num
+      
+      // sum -> 0 // will be the sum of all the divisors (temp) 
+
+        // temp = num - 1
+        // repeat while temp is greater than 1. 
+        //    if number % temp == 0 
+        //        add temp to sum 
+        //    decrease temp by 1. 
+        // if num == sum
+        //    num is a perfect number
+        //    print number
+      //increase num
+
+
+
+
+
+
   }
   
   public static void mersennePrimes() {
